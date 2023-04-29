f4d3f24f044e045ff987e698155f7b8efb5db875
Hongyu Pu <puhongyu1208@gmail.com>
2023-04-29 20:48:56 +0100

    sc

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="about">
-<p class="text-left mx-20 w-30 text-xl text-gray-800">This web app retrieves cocktails based on the ingredients selected using the API provided by <a href='https://www.thecocktaildb.com/api.php' class="text-blue-500">TheCocktailDB</a>. The top part lists three rows of common ingredients along with their images. For any selected set of ingredients, a list of cocktails using these ingredients are queried and displayed. </p>
+<p class="text-left mx-20 w-30 text-xl text-gray-800">This web app retrieves cocktails based on the ingredients selected using the API provided by <a href='https://www.thecocktaildb.com/api.php' class="text-blue-500">TheCocktailDB</a>. The top part lists three rows of common ingredients along with their images. For any selected set of ingredients, a list of cocktails using these ingredients are queried and displayed. To toggle the selection of an ingredient, click the box again. </p>
 
 <br>
 <p class="text-left mx-20 w-30 text-xl text-gray-800">The app is built with Javascript framework <a href='https://vuejs.org/' class="text-blue-500">Vue.js</a>. The API query is handled with <a href='https://axios-http.com/' class="text-blue-500">axios</a>. The styling is configured with <a href='https://tailwindcss.com/' class="text-blue-500">Tailwind CSS</a>. </p>
